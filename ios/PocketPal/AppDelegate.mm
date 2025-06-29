79b1340011660e4452f1ec6f0ef56c5e941594bb
a-ghorbani <ghorbani59@gmail.com>
2025-06-29 20:10:26 +0200

    chore: upgrade rn to 0.80

--- ios/PocketPal/AppDelegate.mm
+++ ios/PocketPal/AppDelegate.mm
@@ -6,6 +6,7 @@
 
 
 #import <React/RCTBundleURLProvider.h>
+#import <ReactAppDependencyProvider/RCTAppDependencyProvider.h>
 #import <RNFSBackgroundDownloads.h>
 
 @implementation AppDelegate
@@ -19,6 +20,7 @@
   [FIRApp configure]; 
 
   self.moduleName = @"PocketPal";
+  self.dependencyProvider = [RCTAppDependencyProvider new];
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
